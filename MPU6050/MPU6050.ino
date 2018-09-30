da8737a68d5387c0c612ec357abeb4110a26fee4
Asck <asck.bbc@gmail.com>
2018-09-30 15:29:50 +0900

    変数名の変更、まとまった処理の関数化と整理

--- MPU6050/MPU6050.ino
+++ MPU6050/MPU6050.ino
@@ -1,72 +1,132 @@
 #include <Wire.h>
 #include <math.h>
 
-void setup() {    
-    Serial.begin(115200);
-    Serial.println("Start");
-    
-    Wire.beginTransmission(0x68);
-    Wire.write(0x1B);//速度のフルスケール設定があるレジスタ
-    Wire.write(0b00000000);//4,3bitを01に指定することで500degree/secまで測れる
-    Wire.endTransmission();
-    
-    Wire.beginTransmission(0x68);
-    Wire.write(0x1C);//加速度のスケールレンジ設定があるレジスタ
-    Wire.write(0b00000000);//4,3bitを00に指定することで+-2gまで測れる。gが重力加速度
-    Wire.endTransmission();
-    
-    Wire.beginTransmission(0x68);
-    Wire.write(0x6B);//クロック設定があるレジスタ
-    Wire.write(0b00000000);//2,1,0bitを000に指定することで、20MHz振動に設定
-    Wire.endTransmission();
-    delay(1000);
+#define MPU_6050_ADDR 0x68 //MPU6050, MPU6000, MPU9250で変わらないはず
+
+uint8_t DIGITAL_8BIT[14] = {0};
+int16_t DIGITAL_16BIT[7] = {0};
+
+typedef struct Vector3 {
+  float x;
+  float y;
+  float z;
 }
 
-uint8_t Data[14] = {0};
-int16_t DData[7] = {0};
-float AData[7] = {0};
-float grad[3] = {0};
+Vector3 ACC = {
+  0, //x
+  0, //y
+  0  //z
+}
+Vector3 GYRO = {
+  0, //x
+  0, //y
+  0  //z
+}
+Vector3 ANGL = {
+  0, //x
+  0, //y
+  0  //z
+}
 
-void loop() {    
-    int static timeA = 0;
-    int static timeB = 0;
-    double static timeDelta = 0;
-    Wire.beginTransmission(0x68);
-    Wire.write(0x3B);
-    Wire.endTransmission();
-    Wire.requestFrom(0x68, 14);
-    uint8_t index = 0;
-    
-    while(Wire.available()){
-       Data[index++] = Wire.read();
-    }
-    //デジタル値の取得
-    DData[0] = (int16_t)(Data[0]<<8 | Data[1]);//x加速度
-    DData[1] = (int16_t)(Data[2]<<8 | Data[3]);//y加速度
-    DData[2] = (int16_t)(Data[4]<<8 | Data[5]);//z加速度
-    DData[3] = (int16_t)(Data[6]<<8 | Data[7]);
-    DData[4] = (int16_t)(Data[8]<<8 | Data[9]);//x角速度
-    DData[5] = (int16_t)(Data[10]<<8 | Data[11]);//y角速度
-    DData[6] = (int16_t)(Data[12]<<8 | Data[13]);//z角速度
-　　//単位はメートル毎秒毎秒[m/s^2]
-    AData[0] = ((float)DData[0]/16383)*9.81;//x加速度
-    AData[1] = ((float)DData[1]/16383)*9.81;//y加速度
-    AData[2] = ((float)DData[2]/16383)*9.81;//z加速度
-　　//単位はdegree毎秒[degree/s]
-    AData[4] = (float)DData[4]/131.072;//x角速度
-    AData[5] = (float)DData[5]/131.072;//y角速度
-    AData[6] = (float)DData[6]/131.072;//z角速度
-    
-    Serial.print(" ACC[g]:  ");
-    Serial.print(AData[0]);    Serial.print("\t");
-    Serial.print(AData[1]);    Serial.print("\t");
-    Serial.print(AData[2]);    Serial.print("\t");
-    
-    Serial.print("GYRO[deg/s]:  ");
-    Serial.print(AData[4]);    Serial.print("\t");
-    Serial.print(AData[5]);    Serial.print("\t");
-    Serial.print(AData[6]);    Serial.print("\t");
-    
-    Serial.print("\n");
-    delay(10);
+void setRegister(uint8_t slaveAddr,uint8_t registAddr,uint8_t registValue) {
+  Wire.beginTransmission(slaveAddr);
+  Wire.write(registerAddr);
+  Wire.write(registValue);
+  Wire.endTransmission();
+}
+void getDigital() {
+  int requestDataLen = 14;
+  Wire.beginTransmission(0x68);
+  Wire.write(0x3B);
+  Wire.endTransmission();
+  //データリクエスト
+  Wire.requestFrom(0x68, requestDataLen);
+  int i = 0;
+
+  //デジタル値の取得
+  while (Wire.available()) {
+    DIGITAL_DATA[i++] = Wire.read();
+  }
+
+  //デジタル値8bitの連結
+  for (i = 0; i < (requestDataLen / 2); i++) {
+    DIGITAL_16BIT[i] = (int16_t)(DIGITAL_8BIT[i * 2] << 8  | DIGITAL_8BIT[i * 2 + 1])
+  }
+}
+
+void DAConvert() {
+  //単位はメートル毎秒毎秒[m/s^2]
+  ACC.x = ((float)DIGITAL_16BIT[0] / 16384) * 9.81; //x加速度
+  ACC.y = ((float)DIGITAL_16BIT[1] / 16384) * 9.81; //y加速度
+  ACC.z = ((float)DIGITAL_16BIT[2] / 16384) * 9.81; //z加速度
+  //単位はdegree毎秒[degree/s]
+  GYRO.x = (float)DIGITAL_16BIT[4] / 131.072; //x角速度
+  GYRO.y = (float)DIGITAL_16BIT[5] / 131.072; //y角速度
+  GYRO.z = (float)DIGITAL_16BIT[6] / 131.072; //z角速度
+}
+
+void calcANGL() {
+  uint8_t static timeNow = 0;
+  uint8_t static timePre = 0;
+  double static timeDelta = 0;// 前回と今の時間の差[s]。積分で使う
+
+  timeNow = micros();
+
+  　//回転速度 * 微小時間[s] で積分
+  ANGL.x += GYRO.x * timeDelta;
+  ANGL.y += GYRO.y * timeDelta;
+  ANGL.z += GYRO.z * timeDelta;
+
+  timePre = timeNow;
+
+  timeDelta = (timeNow - timePre) / pow(10, 6);
+}
+
+void printlnACC() {
+  Serial.print(" ACC[g]:  ");
+  Serial.print(ACC.x[0]);    Serial.print("\t");
+  Serial.print(ACC.y[1]);    Serial.print("\t");
+  Serial.print(ACC.z[2]);    Serial.print("\t");
+}
+void printlnGYRO () {
+  Serial.print("GYRO[deg/s]:  ");
+  Serial.print(GYRO.x[4]);    Serial.print("\t");
+  Serial.print(GYRO.y[5]);    Serial.print("\t");
+  Serial.print(GYRO.z[6]);    Serial.print("\t");
+}
+void printlnANGL () {
+  Serial.print("GYRO[deg/s]:  ");
+  Serial.print(GYRO.x[4]);    Serial.print("\t");
+  Serial.print(GYRO.y[5]);    Serial.print("\t");
+  Serial.print(GYRO.z[6]);    Serial.print("\t");
+}
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Start");
+
+  //0x1B : 速度のフルスケール設定があるレジスタ
+  //0b00000000 : 4,3bitを01に指定することで250degree/secまで測れる
+  setRegister(MPU_6050_ADDR, 0x1B , 0b00000000);
+
+  //0x1C : 加速度のスケールレンジ設定があるレジスタ
+  //0b00000000 : 4,3bitを00に指定することで+-2gまで測れる。gが重力加速度
+  setRegister(MPU_6050_ADDR, 0x1C , 0b00000000);
+  
+  //0x6B : クロック設定があるレジスタ
+  //0b00000000 : 2,1,0bitを000に指定することで、20MHz振動に設定
+  setRegister(MPU_6050_ADDR, 0x6B , 0b00000000);
+  
+  delay(500);
+}
+
+void loop() {
+  getDigital();
+  DAConvert();
+  calcANGL();
+
+  //printlnACC();
+  //printlnGYRO();
+  printlnVNGL();
+  Serial.print("\n");
+  delay(10);
 }
